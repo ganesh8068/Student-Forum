26045c0fb2662097d93b1260d265836543e3b918
Ganeshlokhande07 <116879482+ganesh8068@users.noreply.github.com>
2025-12-14 16:22:57 +0530

    Fix import paths for SignUp and SignIn components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import SignUp from "./pages/Signup";
-import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ForgotPassword from "./pages/ForgotPassword";
